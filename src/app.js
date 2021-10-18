082ddd7193fdd1dacff7a401e01d9cd1d412eb65
beyzaviki <beyzasavun@gmail.com>
2021-10-19 00:53:15 +0300

    18 ekim ders sonu

---
+++ src/app.js
@@ -0,0 +1,28 @@
+console.log("merhaba kodlama.io")
+
+let dolarDun = 9.20
+let dolarBugun = 9.30
+{
+    let dolarDun = 9.10
+}
+console.log(dolarDun)
+
+const euroDun = 11.2
+//euroDun = 11//hata burası
+console.log(euroDun)
+//array
+//camelCasing
+//pascalCasing
+let konutKredileri =["konut kredisi","emlak konut kredisi","kamu konut kredisi"]
+
+console.log("<ul>")
+for(let i = 0;i<konutKredileri.length;i++){
+console.log("<li>"+konutKredileri[i]+"</li>")
+}
+console.log("</ul>")
+{/* <ul>
+    <li>konut kredisi</li>
+    <li>emlak konut kredisi</li>
+    <li>kamu konut kredisi</li>
+    </ul> */}
+console.log(konutKredileri)
